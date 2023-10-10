5fa455689c45acfadddb99d9bb5433e744331129
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2023-10-10 12:47:30 -0700

    Updated bg color layout (#20)
    Co-authored-by: Sean Quiambao <91030482+toastmeal@users.noreply.github.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-code-black`}>{children}</body>
     </html>
   );
 }
