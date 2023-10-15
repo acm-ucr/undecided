f88d582ea3748ec22ca38278c3d3096a19ac17d7
seanquiambao <slquiambao03@gmail.com>
2023-10-14 17:20:04 -0700

    added landing page

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,5 +1,31 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "@/components/Button";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Row className="flex justify-center items-center h-screen w-screen text-center">
+      <Col xl={8}>
+        <div className="w-3/4 items-center mb-12 text-4xl inline-block text-left text-code-white">
+          <div>Hello World!</div>
+          <div className="text-8xl font-bold py-5 leading-normal">
+            Introducing Apollo
+          </div>
+          <div>
+            An educational tool for{" "}
+            <span className="text-code-pink">beginners</span> in computer
+            science to practice their fundamentals!
+          </div>
+        </div>
+      </Col>
+      <Col xl={3}>
+        <div className="flex justify-end">
+          <Button text="Get Started" color="lime" />
+        </div>
+      </Col>
+      <Col xl={1}></Col>
+    </Row>
+  );
 };
 
 export default Landing;
