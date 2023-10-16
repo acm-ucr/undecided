189f6a235889fbfc371c70deb556380a06427e24
fardinzam <fardinzaman51@gmail.com>
2023-10-15 22:58:37 -0700

    added search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,27 @@
-const Search = () => {
-  return <div>Search</div>;
+"use client";
+import { BiSearch } from "react-icons/bi";
+
+const Search = ({ value, onChange, placeholder, classes }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchText = e.target[0].search.value;
+    console.log(`Searched: ${searchText}`);
+  };
+
+  return (
+    <div className={`m-[5%] flex flex-row bg-transparent text-base ${classes}`}>
+      <BiSearch className="mt-[10px] ml-3 text-lg text-code-white fixed" />
+      <input
+        name="search"
+        type="text"
+        placeholder={placeholder}
+        className="p-[6px] pl-10 bg-transparent text-code-white placeholder:text-code-white rounded-xl border-2"
+        value={value}
+        onChange={onChange}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  );
 };
 
 export default Search;
