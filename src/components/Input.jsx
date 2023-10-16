923ac1c1c4c91c2f1f0518fde747579ffce2c476
fardinzam <fardinzaman51@gmail.com>
2023-10-15 20:40:53 -0700

    created input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,23 @@
-const Input = () => {
-  return <div>Input</div>;
+"use client";
+
+const Input = ({ value, onChange, name, placeholder, classes }) => {
+  return (
+    <div className={`flex items-center w-full text-white ${classes}`}>
+      <h6 className="text-xs px-1 z-40 bg-code-black flex fixed ml-3 mt-[-33px] select-none">
+        {name}
+      </h6>
+      <div className="w-full">
+        <input
+          name={name}
+          placeholder={placeholder}
+          type="text"
+          className="outline border-white rounded-md text-base bg-transparent py-2 px-3 w-full"
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,5 +1,24 @@
+"use client";
+import Input from "../Input.jsx";
+
 const Register = () => {
-  return <div>Register</div>;
+  const recordUserInfo = (submitEvent) => {
+    submitEvent.preventDefault();
+    const username = submitEvent.target[0].value;
+    console.log(`Username: ${username}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={recordUserInfo}>
+        <Input
+          name={"Username"}
+          placeholder={"Enter a username"}
+          classes={"mt-5 w-[50%] ml-5"}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
