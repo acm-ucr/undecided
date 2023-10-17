b8b34e23c7e755fdea4cc88ed3b372ed7d32a5f1
Andrew Khadder <andrewkh15@gmail.com>
2023-10-17 11:17:58 -0700

    fixed height

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BiSolidDownArrow } from "react-icons/bi";
+import { BiSolidUpArrow } from "react-icons/bi";
 import { BsDot } from "react-icons/bs";
 import Dropdown from "react-bootstrap/Dropdown";
 import { useState } from "react";
@@ -9,10 +9,10 @@ const Toggle = ({ onClick, show, currentOption, placeholder }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-code-darkgray text-code-white w-1/6 h-10 rounded-lg flex items-center justify-between px-3 py-3"
+      className="bg-code-darkgray text-code-white w-1/6 py-2 rounded-lg flex items-center justify-between px-3"
     >
       {currentOption || placeholder}
-      <BiSolidDownArrow
+      <BiSolidUpArrow
         className={`${
           show && "rotate-180"
         } duration-300 text-code-white text-xs`}
@@ -37,6 +37,7 @@ const Select = ({ currentOption, setCurrentOption, placeholder, options }) => {
         placeholder={placeholder}
         as={Toggle}
       />
+      <div className="p-1" />
       <Dropdown.Menu className="!bg-code-darkgray w-1/6 pt-10">
         {options.map((option, index) => (
           <Dropdown.Item
