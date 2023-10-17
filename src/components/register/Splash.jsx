0704f0ce1278acddd75e0aca002a059e59025dff
Tiffany <twang032302@gmail.com>
2023-10-17 15:12:18 -0700

    remove comment, use p tag, changed to link

--- src/components/register/Splash.jsx
+++ src/components/register/Splash.jsx
@@ -1,18 +1,18 @@
-// Splash.jsx
-import React from "react";
+import Link from "next/link";
 
 const Splash = () => {
   return (
-    <div className="splash-container bg-code-black text-code-white min-h-screen flex flex-col items-center justify-center">
-      <h1 className="splash-title text-4xl mb-4 text-code-white">
-        Congratulations!
-      </h1>
-      <h2 className="splash-welcome text-2xl mb-8">
+    <div className="bg-code-black text-code-white min-h-screen flex flex-col items-center justify-center">
+      <p className="text-4xl mb-4 text-code-white">Congratulations!</p>
+      <p className="text-2xl mb-8">
         Welcome to <span className="text-code-pink">Apollo</span>!
-      </h2>
-      <button className="splash-button py-3 px-4 bg-code-purple text-code-white rounded-full">
+      </p>
+      <Link
+        href="/dashboard"
+        className="py-3 px-4 bg-code-purple text-code-white rounded-full"
+      >
         Go to Dashboard
-      </button>
+      </Link>
     </div>
   );
 };
