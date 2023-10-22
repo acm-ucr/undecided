e65c1e181775152a09bfb59e89ce9efeb5ab90f0
fardinzam <fardinzaman51@gmail.com>
2023-10-22 16:18:53 -0700

    changed submit to comp lvl

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,17 +1,21 @@
-const Input = ({ value, onChange, name, placeholder, classes, type }) => {
+const Input = ({ submitHandler, name, placeholder, classes, type }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Form data: ${e.target[0].value}`);
+    submitHandler(e.target[0].value);
+  };
+
   return (
     <div className={`flex items-center w-1/2 text-white ${classes}`}>
       <p className="text-xs px-1 bg-code-black absolute ml-3 mb-[3%]">{name}</p>
-      <div>
+      <form onSubmit={handleSubmit}>
         <input
           name={name}
           placeholder={placeholder}
           type={type}
           className="outline border-white rounded-md text-base bg-transparent py-2 px-3 w-full"
-          value={value}
-          onChange={onChange}
         />
-      </div>
+      </form>
     </div>
   );
 };
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,31 +1,20 @@
 "use client";
 import Input from "../Input.jsx";
-import { useState } from "react";
 
 const USERNAME_FIELD = "Username";
 
 const Register = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`Form data: ${JSON.stringify(data)}`);
+  const usernameSubmitHandler = (userInput) => {
+    console.log(`Username in register: ${userInput}`);
   };
-  const [data, setData] = useState({ [USERNAME_FIELD]: "" });
-
-  const handleChange = (event, fieldName) => {
-    setData({ ...data, [fieldName]: event.target.value });
-  };
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <Input
-          name={USERNAME_FIELD}
-          placeholder="Enter a username"
-          classes="mt-5 w-1/2 ml-5"
-          value={data[USERNAME_FIELD]}
-          onChange={(e) => handleChange(e, USERNAME_FIELD)}
-        />
-      </form>
+      <Input
+        name={USERNAME_FIELD}
+        placeholder="Enter a username"
+        classes="mt-5 w-1/2 ml-5"
+        submitHandler={usernameSubmitHandler}
+      />
     </div>
   );
 };
