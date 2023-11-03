4e542cfacbd8b81469702cf4e667adc459bb38cf
Fardin Zaman <55073341+fardinzam@users.noreply.github.com>
2023-11-03 16:51:57 -0700

    created input component (#28)
    * created input component
    
    * addressed comments
    
    * fixed label styles
    
    * addressed issues
    
    * changed label position
    
    * changed submit to comp lvl
    
    * added usestate, removed classes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,28 @@
-const Input = () => {
-  return <div>Input</div>;
+"use client";
+import { useState } from "react";
+
+const Input = ({ onSubmit, name, placeholder, type }) => {
+  const [value, setValue] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(value);
+  };
+
+  return (
+    <div className={`flex items-center w-full text-white`}>
+      <p className="text-xs px-1 bg-code-black absolute ml-3 mb-[3%]">{name}</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          name={name}
+          placeholder={placeholder}
+          type={type}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="outline border-white rounded-md text-base bg-transparent py-2 px-3"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Input;
