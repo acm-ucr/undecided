9853b1f90853374c9c883773a38c757b6cced745
Suhani Chaudhary <suhanichaudhary@ucr-secure-42-10-13-218-128.wnet.ucr.edu>
2023-11-03 16:47:56 -0700

    Clean up div components

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -4,22 +4,20 @@ import { BsStarFill } from "react-icons/bs";
 
 const Splash = ({ runs, points }) => {
   return (
-    <div className="flex flex-col w-full h-screen bg-code-darkgray absolute">
+    <div className="flex flex-col w-full h-screen bg-code-darkgray">
       <div className="flex relative w-full h-1/3 justify-center items-end">
-        <div className="w-20 h-1/2">
-          <FaPlus className=" text-code-white w-20 h-20 mt-3" />
-        </div>
+        <FaPlus className="text-code-white w-20 h-20 mt-3" />
         <div className="flex relative ml-5">
-          <BsStarFill className="text-code-lightorange  w-48 h-48" />
-          <div className="absolute top-1/2 left-1/2 -translate-x-5 -translate-y-3 text-3xl text-code-darkgray font-semibold">
+          <BsStarFill className="text-code-lightorange w-48 h-48" />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-code-darkgray font-semibold">
             {points}
           </div>
         </div>
       </div>
       <div className="flex flex-col mt-5 w-full items-center">
-        <text className=" text-4xl  font-semibold text-code-white">
+        <p className="text-4xl  font-semibold text-code-white">
           Congratulations!
-        </text>
+        </p>
         <text className="h-1/3 text-2xl text-code-white text-center m-3">
           You passed all test cases within {runs} runs!
         </text>
@@ -27,13 +25,13 @@ const Splash = ({ runs, points }) => {
       <div className="flex flex-col space-y-3 w-full h-1/4 items-center justify-center">
         <Link
           href="/profile"
-          className=" bg-code-lime rounded-full w-1/4 text-center py-2 no-underline"
+          className="bg-code-lime rounded-full w-1/4 text-center py-2 no-underline"
         >
           My Stars
         </Link>
         <Link
           href="/"
-          className=" bg-code-black text-code-white rounded-full w-1/4 text-center py-2 px-4 no-underline"
+          className="bg-code-black text-code-white rounded-full w-1/4 text-center py-2 px-4 no-underline"
         >
           Post this Solution
         </Link>
