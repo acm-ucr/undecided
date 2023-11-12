1bcd5acb7c4949ad58ebcd2ef55d07781021e128
fardinzam <fardinzaman51@gmail.com>
2023-11-11 20:10:50 -0800

    fixed register component issues

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,16 +1,21 @@
+"use client";
 import Button from "@/components/Button";
 import Input from "../Input";
 
 const Register = ({ name }) => {
   return (
-    <div className="h-[calc(100vh-63.516px)] flex items-center">
+    <div className="h-screen flex items-center">
       <div className="w-full flex flex-row justify-between mx-[5%]">
         <div>
           <p className="text-white text-3xl mb-5">
             Hello <span className="text-[#dd8ab8]">{name}</span>, <br /> Create
             your username!
           </p>
-          <Input name="Username" placeholder="Enter a username" />
+          <Input
+            name="Username"
+            placeholder="Enter a username"
+            onSubmit={(val) => console.log(val)}
+          />
         </div>
         <div className="h-[90%] my-auto">
           <Button text="Get Started" color="lime" />
