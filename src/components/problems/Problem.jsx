8763a3af6963e70347d1188248cd78eb3c966a0d
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2023-11-30 02:56:15 -0800

    solutions scrollable (#52)
    * solutions scrollable
    
    * made tabs sticky

--- src/components/problems/Problem.jsx
+++ src/components/problems/Problem.jsx
@@ -45,8 +45,8 @@ const Problem = () => {
 
   return (
     <div className="w-11/12 flex justify-center text-white h-[90%] gap-4">
-      <div className="w-1/2 rounded-md bg-code-darkgray">
-        <div className="flex mt-3 text-sm border-b-2 border-code-gray">
+      <div className="w-1/2 rounded-md bg-code-darkgray overflow-scroll">
+        <div className="flex text-sm border-b-2 border-code-gray sticky bg-code-darkgray top-0 z-1 pt-3">
           {TABS.map(({ title, value }, index) => (
             <p
               key={index}
